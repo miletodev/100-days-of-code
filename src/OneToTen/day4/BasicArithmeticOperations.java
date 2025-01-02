a04598545dbf3ef781ffe0aba2ac1582fe557f35
Mileto Almeida <almeidamileto@gmail.com>
2025-01-02 20:45:17 -0300

    Feat: Day 4 - Implements Basic Arithmetic Operations

--- src/OneToTen/day4/BasicArithmeticOperations.java
+++ src/OneToTen/day4/BasicArithmeticOperations.java
@@ -6,6 +6,35 @@ public class BasicArithmeticOperations {
     public static void main (String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Enter the option for the operation you want to perform: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        int option = keyboard.nextInt();
+        System.out.println("You chose option " + option);
+        System.out.println("Enter the first number: ");
+        int num1 = keyboard.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = keyboard.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("The sum of the two numbers is: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("The difference of the two numbers is: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("The product of the two numbers is: " + (num1 * num2));
+                break;
+            case 4:
+                System.out.println("The quotient of the two numbers is: " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+
 
     }
 }
