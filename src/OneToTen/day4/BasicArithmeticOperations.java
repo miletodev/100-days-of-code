3e3f73b4b2b24c17ac6e9af81d54cf7fa74fbdeb
Mileto Almeida <almeidamileto@gmail.com>
2025-01-02 19:11:41 -0300

    Feat: Day 4 - Basic Arithmetic Operations

---
+++ src/OneToTen/day4/BasicArithmeticOperations.java
@@ -0,0 +1,11 @@
+package OneToTen.day4;
+
+import java.util.Scanner;
+
+public class BasicArithmeticOperations {
+    public static void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+
+    }
+}
